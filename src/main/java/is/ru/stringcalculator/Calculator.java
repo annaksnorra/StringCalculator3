0cff372d54b0ed1a7aa77b1405ccafa235630edc
annaksnorra <annaksnorra@gmail.com>
2014-10-18 00:17:48 +0000

    leyfa different delimeter ad splitta strengnum

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -3,11 +3,15 @@ package is.ru.stringcalculator;
 public class Calculator {
 
 	public static int add(String text){
-		if(text.contains("\n") && text.contains(",")) {
-			return sum(splitMultiple(text));
-		}
-		else if(text.equals("")){
+		
+		if(text.equals("")){
 			return 0;
+		}
+		else if(text.contains("//"))  {
+			return sum(splitCustomDelimeter(text));
+		}
+		else if(text.contains("\n") && text.contains(",")) {
+			return sum(splitMultiple(text));
 		}
 		else if(text.contains(",")){
 			return sum(splitNumbers(text));
@@ -36,7 +40,13 @@ public class Calculator {
     	String[] simbols = numbers.split(splitter);
     		return simbols;
     }
-      
+
+    private static String[] splitCustomDelimeter(String numbers) {
+    	String[] array = newLine(numbers);
+    	String delimeter = array[0].substring(2);
+    		return array[1].split(delimeter);
+    }
+
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
@@ -44,7 +54,5 @@ public class Calculator {
 		}
 		return total;
     }
-
-
 
 }
